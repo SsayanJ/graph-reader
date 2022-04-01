759a11573a5ce6e8e2e9ff213d422bbe8f605e11
Jeff <jeff@eyepick.co>
2022-04-01 02:02:21 +0200

    created GraphReader class

--- graph_reader.py
+++ graph_reader.py
@@ -5,8 +5,82 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 from logging import Logger
+import os
 
 logger = Logger("Graph Reader", level="DEBUG")
+
+
+class GraphReader():
+    def __init__(self) -> None:
+        self.source_name = None
+
+    def from_image(self, image_path, nb_points=100):
+        self.source_name = os.path.basename(image_path)
+        self.color_image = cv2.imread(image_path)
+        self.curve_roi, self.x_axis_roi, self.y_axis_roi = self.get_all_rois(self.color_image)
+        self.nb_curves = self.get_curve_number(self.curve_roi)
+        self.mask_curve_roi = self.compute_kmeans(self.curve_roi, self.nb_curves+1, use_HSV=True)
+        self.curve_points = self.read_curves(self.mask_curve_roi, nb_points)
+    
+    def read_curves(self, mask_curve_roi, nb_points=100):
+        curve_points = None
+        return curve_points 
+    
+    def read_single_curve(self, curve_binary_mask, nb_points=100):
+        
+        return x_points, y_points
+    
+    def get_curve_point_from_binary(image, x):
+        min_indexes = np.where(image[:, x] == 1)
+        return int(np.mean(min_indexes))
+
+    def get_all_rois(image):
+
+        grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(grey_img, 50, 150, apertureSize=3)
+        # Find contours
+        contours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+
+        biggest_rectangle = None
+        min_area = image.shape[0] * image.shape[1] / 4
+        for cnt in contours:
+            epsilon = 0.05 * cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, epsilon, True)
+            if len(approx) == 4 and cv2.contourArea(cnt) > min_area:
+                biggest_rectangle = np.squeeze(approx)
+
+        x_min, y_min = np.min(biggest_rectangle, axis=0)
+        x_max, y_max = np.max(biggest_rectangle, axis=0)
+
+        margin = 3
+        axis_margin = 5
+        curve_roi = image[y_min + margin : y_max - margin, x_min + margin : x_max - margin]
+        x_axis_roi = image[-(image.shape[0]-y_max) - axis_margin :, :]
+        y_axis_roi = image[:, : x_min + axis_margin]
+
+        return curve_roi, x_axis_roi, y_axis_roi
+    
+    def compute_kmeans(curve_roi, nb_clusters, use_HSV=True):
+        if use_HSV:
+            curve_roi = cv2.cvtColor(curve_roi, cv2.COLOR_BGR2HSV)
+        X = curve_roi.reshape(curve_roi.shape[0] * curve_roi.shape[1], curve_roi.shape[2])
+        result = KMeans(n_clusters=nb_clusters).fit_predict(X)
+        return result.reshape(curve_roi.shape[:2])
+
+
+    def count_curves(gray_roi, x):
+        current_pixel = 255
+        nb_curves = 0
+        for pixel in gray_roi[:, x]:
+            if current_pixel > 250 and pixel < 240:
+                nb_curves += 1
+            current_pixel = pixel
+        logger.debug("x", x, "nb_curves:", nb_curves)
+        return nb_curves
+
+    def get_curve_number(self, curve_roi):
+        grey_roi = cv2.cvtColor(curve_roi, cv2.COLOR_BGR2GRAY)
+        return max([self.count_curves(grey_roi, int(i * grey_roi.shape[1] / 6)) for i in range(2, 6)])
 
 
 def get_curve_point(image, x):
@@ -94,25 +168,4 @@ def get_all_rois(image):
     return curve_roi, x_axis_roi, y_axis_roi
 
 
-def compute_kmeans(roi, nb_clusters, use_HSV=True):
-    if use_HSV:
-        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    X = roi.reshape(roi.shape[0] * roi.shape[1], roi.shape[2])
-    result = KMeans(n_clusters=nb_clusters).fit_predict(X)
-    return result.reshape(roi.shape[:2])
 
-
-def count_curves(gray_roi, x):
-    current_pixel = 255
-    nb_curves = 0
-    for pixel in gray_roi[:, x]:
-        if current_pixel > 250 and pixel < 240:
-            nb_curves += 1
-        current_pixel = pixel
-    logger.debug("x", x, "nb_curves:", nb_curves)
-    return nb_curves
-
-
-def get_curve_number(roi):
-    grey_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    return max([count_curves(grey_roi, int(i * grey_roi.shape[1] / 6)) for i in range(2, 6)])
